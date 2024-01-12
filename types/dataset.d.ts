dcc0496f5166052e658dac5ee35cb4fa2b8c408b
Sanjay Soundarajan <sanjay.16@live.com>
2024-01-11 17:03:47 -0800

    🏷️ feat: add dataset_description type definition

--- types/dataset.d.ts
+++ types/dataset.d.ts
@@ -1,3 +1,167 @@
+interface StudyDescription {
+  [key: string]: any;
+}
+interface DatasetDescription {
+  Identifier: {
+    identifierValue: string;
+    identifierType: string;
+  };
+  Title: {
+    titleType?: string;
+    titleValue: string;
+  }[];
+  Version: string;
+  AlternateIdentifier?: {
+    alternateIdentifierType: string;
+    alternateIdentifierValue: string;
+  }[];
+  Creator: {
+    creatorName: string;
+    nameType: string;
+    nameIdentifier?: {
+      nameIdentifierValue: string;
+      nameIdentifierScheme: string;
+      schemeURI?: string;
+    }[];
+    affiliation?: {
+      affiliationIdentifier?: string;
+      affiliationIdentifierScheme?: string;
+      schemeURI?: string;
+      affiliationValue?: string;
+    }[];
+  }[];
+  Contributor?: {
+    contributorName: string;
+    contributorType: string;
+    nameType: string;
+    nameIdentifier?: {
+      nameIdentifierValue: string;
+      nameIdentifierScheme: string;
+      schemeURI?: string;
+    }[];
+    affiliation?: {
+      affiliationIdentifier?: string;
+      affiliationIdentifierScheme?: string;
+      schemeURI?: string;
+      affiliationValue?: string;
+    }[];
+  }[];
+  PublicationYear: string;
+  Date?: {
+    dateValue: string;
+    dateType: string;
+    dateInformation?: string;
+  }[];
+  ResourceType: {
+    resourceTypeGeneral: string;
+    resourceTypeValue: string;
+  }[];
+  DatasetDeIdentLevel: {
+    deIdentType: string;
+    deIdentDirect: boolean;
+    deIdentHIPAA: boolean;
+    deIdentDates: boolean;
+    deIdentNonarr: boolean;
+    deIdentKanon: boolean;
+    deIdentDetails?: string;
+  };
+  DatasetConsent: {
+    consentType: string;
+    consentNoncommercial: boolean;
+    consentGeogRestrict: boolean;
+    consentResearchType: boolean;
+    consentGeneticOnly: boolean;
+    consentNoMethods: boolean;
+    consentsDetails?: string;
+  };
+  Description?: {
+    descriptionType: string;
+    descriptionValue: string;
+  }[];
+  Language?: string;
+  RelatedIdentifier?: {
+    relatedIdentifierType: string;
+    relatedIdentifierValue: string;
+    relationType: string;
+    relatedMetadataScheme?: string;
+    schemeURI?: string;
+    schemeType?: string;
+    resourceTypeGeneral?: string;
+  }[];
+  Subject?: {
+    subjectValue: string;
+    subjectScheme?: string;
+    schemeURI?: string;
+    valueURI?: string;
+    classificationCode?: string;
+  }[];
+  ManagingOrganisation: {
+    name: string;
+    rorId?: string;
+  };
+  AcessType: string;
+  AcessDetails: {
+    description: string;
+    url: string;
+    urlLastChecked: string;
+  };
+  Rights: {
+    rightsValue: string;
+    rightsURI?: string;
+    rightsIdentifier?: string;
+    rightsIdentifierScheme?: string;
+  }[];
+  Publisher: string;
+  Size?: string[];
+  FundingReference?: {
+    funderName: string;
+    funderIdentifier?: {
+      funderIdentifierValue: string;
+      funderIdentifierType?: string;
+    };
+    awardNumber?: {
+      awardNumberValue: string;
+      awardURI?: string;
+    };
+    awardTitle?: string;
+  }[];
+  RelatedItem?: {
+    relatedItemType: string;
+    relationType: string;
+    relatedItemIdentifier?: {
+      relatedItemIdentifierValue: string;
+      relatedItemIdentifierType?: string;
+      relatedMetadataScheme?: string;
+      schemeURI?: string;
+      schemeType?: string;
+    }[];
+    creator: {
+      creatorName: string;
+      nameType: string;
+    }[];
+    title: {
+      titleType?: string;
+      titleValue: string;
+    }[];
+    publicationYear?: string;
+    volume?: string;
+    issue?: string;
+    number: {
+      numberType?: string;
+      numberValue: string;
+    };
+    firstPage?: string;
+    lastPage?: string;
+    publisher?: string;
+    edition?: string;
+    contributor?: {
+      contributorName: string;
+      contributorType: string;
+      nameType: string;
+    }[];
+  }[];
+}
+
 interface Dataset {
   id: number;
   updatedOn: number;
@@ -8,10 +172,10 @@ interface Dataset {
   metadata: {
     dataSheet: string;
     readme: string;
-    studyDescription: [key: string]: any ;
-    datasetDescription: [key: string]: any ;
+    studyDescription: StudyDescription;
+    datasetDescription: DatasetDescription;
   };
-};
+}
 
 interface DatasetArrayItem {
   id: number;
