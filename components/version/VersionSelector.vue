c32eee968ac631816c244fcb26c1560e83c077e2
Sanjay Soundarajan <sanjay.16@live.com>
2024-01-15 15:54:24 -0800

    ✨ feat: add support for dynamic versions

---
+++ components/version/VersionSelector.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+const props = defineProps({
+  id: {
+    required: true,
+    type: Number,
+  },
+});
+
+const {
+  data: versions,
+  error,
+  pending,
+} = await useFetch(`/api/versions/${props.id}`, {
+  lazy: true,
+  server: false,
+});
+
+// todo: add a watchEffect for the error responses
+</script>
+
+<template>
+  <n-space
+    vertical
+    class="rounded-xl border border-purple-200 bg-slate-50 pb-5 pt-3"
+  >
+    <n-space vertical :size="[0, 0]">
+      <h3 class="mb-3 px-4">Versions</h3>
+
+      <TransitionFade>
+        <div v-if="pending" class="flex justify-center p-2">
+          <Icon name="svg-spinners:3-dots-scale" size="30" />
+        </div>
+
+        <div v-else>
+          <n-space
+            v-for="version in versions"
+            :key="version.id"
+            justify="space-between"
+            align="start"
+            class="p-2 transition-all hover:bg-purple-50"
+            :class="{
+              '!bg-purple-200': version.id === id,
+            }"
+          >
+            <div class="flex flex-col space-y-1">
+              <NuxtLink
+                :to="`/datasets/${version.id}`"
+                class="text-sm font-medium transition-all hover:text-slate-600 hover:underline"
+              >
+                Version {{ version.title }}
+              </NuxtLink>
+
+              <NuxtLink
+                :to="`https://doi.org/${version.doi}`"
+                target="_blank"
+                class="text-sm transition-all hover:text-slate-600 hover:underline"
+              >
+                {{ version.doi }}
+              </NuxtLink>
+            </div>
+
+            <p class="text-right text-xs text-gray-500">
+              {{ $dayjs.unix(version.createdAt).format("MMM D, YYYY") }}
+            </p>
+          </n-space>
+        </div>
+      </TransitionFade>
+    </n-space>
+  </n-space>
+</template>
