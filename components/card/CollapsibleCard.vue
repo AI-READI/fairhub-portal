7050f911f4a324843ea2b7cb5e3f869ef343e37b
Sanjay Soundarajan <sanjay.16@live.com>
2024-01-22 14:57:54 -0800

    💄 style: update collapsible card styling

--- components/card/CollapsibleCard.vue
+++ components/card/CollapsibleCard.vue
@@ -30,20 +30,20 @@ const toggleCollapse = () => {
 <template>
   <div class="rounded-lg border border-slate-300 bg-white shadow-lg">
     <div
-      class="x flex justify-between rounded-lg px-6 pb-6 pt-6 transition-all"
+      class="x flex items-center justify-between rounded-lg px-6 py-3 transition-all"
       :class="{
         'bg-white': !collapseContent,
         'bg-slate-50/50': collapseContent,
       }"
     >
-      <div class="text-xl font-semibold leading-9">{{ title }}</div>
+      <div class="text-xl font-bold">{{ title }}</div>
 
       <div class="flex items-center">
         <slot name="header-extra"></slot>
 
         <n-button
           text
-          class="pl-5 text-3xl"
+          class="rounded-full p-2 text-3xl transition-all hover:!bg-cyan-100"
           type="info"
           @click="toggleCollapse"
         >
