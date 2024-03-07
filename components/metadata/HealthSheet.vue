c70ae8428924a632207c3c370535b2bb09222aaf
Sanjay Soundarajan <sanjay.16@live.com>
2024-03-07 13:02:33 -0800

    ♻️ refactor: remove repetitions

--- components/metadata/HealthSheet.vue
+++ components/metadata/HealthSheet.vue
@@ -1,101 +1,53 @@
 <script setup lang="ts">
-defineProps({
+const props = defineProps({
   healthsheet: {
     required: true,
     type: Object as PropType<HealthsheetRecords>,
   },
 });
+
+const healthsheetData = [
+  {
+    title: "Motivation",
+    data: props.healthsheet.motivation,
+  },
+  {
+    title: "Composition",
+    data: props.healthsheet.composition,
+  },
+  {
+    title: "Collection",
+    data: props.healthsheet.collection,
+  },
+  {
+    title: "Preprocessing / Cleaning / Labeling",
+    data: props.healthsheet.preprocessing,
+  },
+  {
+    title: "Uses",
+    data: props.healthsheet.uses,
+  },
+  {
+    title: "Distribution",
+    data: props.healthsheet.distribution,
+  },
+  {
+    title: "Maintenance",
+    data: props.healthsheet.maintenance,
+  },
+];
 </script>
 
 <template>
   <n-space vertical size="large">
-    <CardCollapsibleCard title="Motivation" :collapse="false">
+    <CardCollapsibleCard
+      v-for="(rootItem, index) in healthsheetData"
+      :key="index"
+      :title="rootItem.title"
+      :collapse="index > 0"
+    >
       <n-space vertical size="large">
-        <div v-for="item in healthsheet.motivation" :key="item.id">
-          <h3 class="mb-1 w-full border-b text-base font-semibold">
-            {{ item.question }}
-          </h3>
-
-          <p v-if="item.response.trim()">{{ item.response }}</p>
-
-          <p v-else class="italic">N/A</p>
-        </div>
-      </n-space>
-    </CardCollapsibleCard>
-
-    <CardCollapsibleCard title="Composition">
-      <n-space vertical size="large">
-        <div v-for="item in healthsheet.composition" :key="item.id">
-          <h3 class="mb-1 w-full border-b text-base font-semibold">
-            {{ item.question }}
-          </h3>
-
-          <p v-if="item.response.trim()">{{ item.response }}</p>
-
-          <p v-else class="italic">N/A</p>
-        </div>
-      </n-space>
-    </CardCollapsibleCard>
-
-    <CardCollapsibleCard title="Collection">
-      <n-space vertical size="large">
-        <div v-for="item in healthsheet.collection" :key="item.id">
-          <h3 class="mb-1 w-full border-b text-base font-semibold">
-            {{ item.question }}
-          </h3>
-
-          <p v-if="item.response.trim()">{{ item.response }}</p>
-
-          <p v-else class="italic">N/A</p>
-        </div>
-      </n-space>
-    </CardCollapsibleCard>
-
-    <CardCollapsibleCard title="Preprocessing / Cleaning / Labeling">
-      <n-space vertical size="large">
-        <div v-for="item in healthsheet.preprocessing" :key="item.id">
-          <h3 class="mb-1 w-full border-b text-base font-semibold">
-            {{ item.question }}
-          </h3>
-
-          <p v-if="item.response.trim()">{{ item.response }}</p>
-
-          <p v-else class="italic">N/A</p>
-        </div>
-      </n-space>
-    </CardCollapsibleCard>
-
-    <CardCollapsibleCard title="Uses">
-      <n-space vertical size="large">
-        <div v-for="item in healthsheet.uses" :key="item.id">
-          <h3 class="mb-1 w-full border-b text-base font-semibold">
-            {{ item.question }}
-          </h3>
-
-          <p v-if="item.response.trim()">{{ item.response }}</p>
-
-          <p v-else class="italic">N/A</p>
-        </div>
-      </n-space>
-    </CardCollapsibleCard>
-
-    <CardCollapsibleCard title="Distribution">
-      <n-space vertical size="large">
-        <div v-for="item in healthsheet.distribution" :key="item.id">
-          <h3 class="mb-1 w-full border-b text-base font-semibold">
-            {{ item.question }}
-          </h3>
-
-          <p v-if="item.response.trim()">{{ item.response }}</p>
-
-          <p v-else class="italic">N/A</p>
-        </div>
-      </n-space>
-    </CardCollapsibleCard>
-
-    <CardCollapsibleCard title="Maintenance">
-      <n-space vertical size="large">
-        <div v-for="item in healthsheet.maintenance" :key="item.id">
+        <div v-for="item in rootItem.data" :key="item.id">
           <h3 class="mb-1 w-full border-b text-base font-semibold">
             {{ item.question }}
           </h3>
