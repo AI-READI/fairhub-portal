56b0cb384b94f2ac947266cdc052417a0826bac4
Heath Harrelson <harrelst@ohsu.edu>
2024-03-18 16:47:34 -0700

    🐛 fix: add scopes to get user details with login (#41)
    Add missing scopes to get user details in the login response.

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 const { accounts, inProgress, instance } = useMsal();
 
-const signupRedirect = () => {
-  instance.loginRedirect();
+const loginRedirect = () => {
+  instance.loginRedirect({ scopes: ["openid", "email", "profile"] });
+};
+
+const logout = () => {
+  instance.logoutRedirect();
 };
 </script>
 
@@ -13,8 +17,27 @@ const signupRedirect = () => {
 
       <VueJsonPretty :data="accounts" />
 
-      <div>
-        <n-button secondary type="info" @click="signupRedirect">
+      <div v-if="accounts.length">
+        <n-descriptions
+          v-for="account in accounts"
+          :key="account.localAccountId"
+          label-placement="top"
+          :title="account.localAccountId"
+        >
+          <n-descriptions-item label="Username">
+            {{ account.idTokenClaims.preferred_username }}
+          </n-descriptions-item>
+
+          <n-descriptions-item label="Email">
+            {{ account.idTokenClaims.email }}
+          </n-descriptions-item>
+        </n-descriptions>
+
+        <n-button secondary type="info" @click="logout">Log Out</n-button>
+      </div>
+
+      <div v-else>
+        <n-button secondary type="info" @click="loginRedirect">
           Sign Up and Sign In
         </n-button>
       </div>
