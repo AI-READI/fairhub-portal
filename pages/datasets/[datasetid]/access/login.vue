d2b7c78a154f8f8de9ee8330672ba35989244a47
Erik Benton <benton@ohsu.edu>
2024-05-03 14:12:50 -0700

    fix: instructions for login

--- pages/datasets/[datasetid]/access/login.vue
+++ pages/datasets/[datasetid]/access/login.vue
@@ -140,6 +140,15 @@ const handleSubmit = async () => {
             </div>
 
             <div v-else>
+              <n-alert title="Please note:" type="info">
+                <p>
+                  When registering for access you must use your organization or
+                  institutional email address. Accounts not affiliated with a
+                  recognized entity will be dismissed. AI-READI admins will
+                  confirm your identity before granting access to the dataset.
+                </p>
+              </n-alert>
+
               <n-button
                 size="large"
                 type="info"
