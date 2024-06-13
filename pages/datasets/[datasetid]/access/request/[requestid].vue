aed5e63aec93a1a6e3b78803092c5817e497c663
Heath Harrelson <harrelst@ohsu.edu>
2024-06-13 14:54:31 -0700

    ✨ feat: improved download instructions (#127)
    * ✨ feat: improved download instructions
    
    Improve download instructions:
    
    * remove command line sftp instructions
    * add FileZilla download instructions
    * attempt to fix SSH key download
    * ensure that SSH key ends with a newline
    
    Fixes #115, #117, and #119 .
    
    ---------
    
    Co-authored-by: Erik Benton <kirenotneb@users.noreply.github.com>

--- pages/datasets/[datasetid]/access/request/[requestid].vue
+++ pages/datasets/[datasetid]/access/request/[requestid].vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { useClipboard } from "@vueuse/core";
-
 definePageMeta({
   middleware: ["auth"],
 });
@@ -20,16 +18,14 @@ const user = userDetails.value;
 const request = req.value as any;
 
 const userKey = user.email.replace(/[^a-zA-Z0-9]/g, "");
+const [username, host] = request?.download_uri?.split("@");
 
 const notUserDownload = user.id !== request.user_details_id;
 const dataReady = request.status === "READY";
 const isExpired = request.status === "EXPIRED";
 
-const data = request.download_key as string;
-const byteString = atob(data) + "\n";
-
-const source = ref(byteString);
-const { copied, copy, isSupported, text } = useClipboard({ source });
+const privateKeyUrl = `/api/downloads/key/${requestid}`;
+const privateKeyFilename = `${userKey}.txt`;
 
 if (error.value) {
   console.error(error.value);
@@ -86,40 +82,93 @@ if (error.value) {
 
           <div v-else-if="dataReady">
             <p>
-              Your dataset is ready for download. Please use the following
-              address and SSH key to access the data. Copy the SSH key and save
-              it in a secure location. Make sure that the file permissions are
-              set read-only for your account.
+              Your dataset is ready for download. Due to the number and size of
+              the files included in typical datasets, we recommend using a
+              graphical client like
+              <NuxtLink to="https://filezilla-project.org/" target="_blank"
+                >FileZilla</NuxtLink
+              >. FileZilla is a free and open-source, cross-platform FTP
+              application. It is a very mature SFTP application and is
+              specifically supported for SFTP applications by Microsoft Azure
+              Storage.
             </p>
 
             <p>
-              Depending on your computer system and comfort you can either use a
-              client like
-              <NuxtLink
-                :to="`https://filezilla-project.org/`"
-                :target="`_blank`"
-                >Filezilla</NuxtLink
-              >
-              or the command line. See below for an example using the command
-              line:
+              Detailed instructions on FileZilla, installation and use are
+              available from the
+              <a href="https://wiki.filezilla-project.org/Documentation"
+                >official project wiki</a
+              >. Below is a brief summary of steps you'll need to accomplish
+              your download:
             </p>
 
-            <pre>sftp -i {{ userKey }}.key {{ request?.download_uri }}</pre>
+            <ol>
+              <li>
+                Download your SSH key using the link below, and save it in a
+                secure location. Make sure that the file permissions are set
+                read-only for your account.
 
-            <h4>SSH Key</h4>
+                <ul>
+                  <li>
+                    <NuxtLink
+                      :to="privateKeyUrl"
+                      :download="privateKeyFilename"
+                      external
+                    >
+                      Download SSH key
+                      <Icon name="mdi:file-download-outline" class="ml-1"
+                    /></NuxtLink>
+                  </li>
+                </ul>
+              </li>
 
-            <div v-if="isSupported">
-              <n-button strong secondary type="info" @click="copy(source)">
-                <!-- by default, `copied` will be reset in 1.5s -->
-                <span v-if="!copied">Copy key</span>
+              <li>
+                Download the version of the FileZilla Client compatible with
+                your operating system from the
+                <a href="https://filezilla-project.org/download.php"
+                  >official download page</a
+                >.
+              </li>
 
-                <span v-else>Copied!</span>
-              </n-button>
-            </div>
+              <li>
+                Install the FileZilla client according to the
+                <a href="https://wiki.filezilla-project.org/Client_Installation"
+                  >installation instructions</a
+                >
+                for your operating system.
+              </li>
 
-            <p v-else>Your browser does not support Clipboard API</p>
+              <li>
+                Follow the
+                <a href="https://wiki.filezilla-project.org/Using"
+                  >usage instructions</a
+                >
 
-            <pre ref="key">{{ byteString }}</pre>
+                to download your requested data set. You should use the
+                following properties to set up the connection in the
+                <a href="https://wiki.filezilla-project.org/Site_Manager"
+                  >FileZilla Site Manager</a
+                >:
+
+                <ul>
+                  <li>
+                    <em>Protocol:</em> Select "SFTP - SSH File Transfer
+                    Protocol"
+                  </li>
+
+                  <li><em>Host:</em> {{ host }}</li>
+
+                  <li><em>Logon type:</em> Select "Key file"</li>
+
+                  <li><em>User:</em> {{ username }}</li>
+
+                  <li>
+                    <em>Key file:</em> The location where you saved the SSH key
+                    file (see above)
+                  </li>
+                </ul>
+              </li>
+            </ol>
           </div>
 
           <!-- Request is not ready -->
