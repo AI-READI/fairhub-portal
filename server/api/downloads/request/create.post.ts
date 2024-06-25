771e228fe04b29f6ea3e4b239b6c64ee32114a41
Heath Harrelson <harrelst@ohsu.edu>
2024-06-25 09:53:07 -0700

    🔨 chore: download request approval
    Add state to store whether a download request was approved.

--- server/api/downloads/request/create.post.ts
+++ server/api/downloads/request/create.post.ts
@@ -26,7 +26,7 @@ export default defineEventHandler(async (event) => {
     licenseAgreement.dataset_id.trim() !== postBody.dataset_id.trim() ||
     licenseAgreement.user_details_id !== userDetails.id
   ) {
-    console.warn(
+    console.error(
       "License agreement not found, or it doesn't match the dataset and user",
     );
     throw createError({
@@ -37,22 +37,35 @@ export default defineEventHandler(async (event) => {
 
   try {
     const timestamp = currentUnixTimestamp();
-    const downloadRequest = await prisma.download_request.create({
-      data: {
-        id: uuid(),
-        created_at: timestamp,
-        dataset_id: postBody.dataset_id,
-        download_agreement_id: licenseAgreement.id,
-        folders_selected: postBody.folders_selected,
-        status: "PENDING",
-        updated_on: timestamp,
-        user_details_id: userDetails.id,
-      },
-      select: selectProps,
-    });
+    const approvalId = uuid();
+    const [_approval, downloadRequest] = await prisma.$transaction([
+      prisma.download_request_approval.create({
+        data: {
+          id: approvalId,
+          approval_status: "AWAITING_APPROVAL",
+          created_at: timestamp,
+          updated_on: timestamp,
+        },
+      }),
+      prisma.download_request.create({
+        data: {
+          id: uuid(),
+          approval_id: approvalId,
+          created_at: timestamp,
+          dataset_id: postBody.dataset_id,
+          download_agreement_id: licenseAgreement.id,
+          folders_selected: postBody.folders_selected,
+          status: "PENDING",
+          updated_on: timestamp,
+          user_details_id: userDetails.id,
+        },
+        select: selectProps,
+      }),
+    ]);
 
     return downloadRequest;
   } catch (error) {
+    console.error(error);
     throw createError({
       statusCode: 422,
       statusMessage: "Download request could not be created",
