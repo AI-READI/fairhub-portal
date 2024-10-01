b4d06a2b27eb7480818d87e1b37fcdc4a9ad814a
Eamon Dysinger <dysingee@ohsu.edu>
2024-09-30 20:27:30 -0700

    Adversarial IdP text updated per request by Shannon McWeeny

--- pages/forbiddenlogin.vue
+++ pages/forbiddenlogin.vue
@@ -9,15 +9,21 @@ const badIdPType = route.query.type;
     <div class="font-dark text-5xl font-bold">403: Forbidden</div>
 
     <p class="text-2xl font-light leading-normal md:text-3xl">
-      We're sorry, but the IdP you have used is not permitted to access project
-      resources.
+      Thank you for your request.
     </p>
 
     <div v-if="badIdPType === 'adversarial'">
       <p class="text-2xl font-light leading-normal md:text-3xl">
         You have been directed to this page because you have authenticated using
         an Identity Provider that is based in a country that has been designated
-        by the U.S. Department of Commerce as a United States Foreign Adversary.
+        a country of concern by the US Government. At this time, we are awaiting
+        clarification from the Department of Justice on the recent executive
+        order on data sharing with countries of concern (<a
+          href="https://www.whitehouse.gov/briefing-room/presidential-actions/2024/02/28/executive-order-on-preventing-access-to-americans-bulk-sensitive-personal-data-and-united-states-government-related-data-by-countries-of-concern/"
+          >Details Here</a
+        >). Please know that we are fully committed to maximizing the impact of
+        the the data from our participants and data sharing but have been
+        advised to for clarification.
       </p>
     </div>
 
