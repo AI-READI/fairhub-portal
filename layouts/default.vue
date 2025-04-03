65290ea1aba32db1e802098a94e616982fde0682
aydawka <aydan.gasimova2@gmail.com>
2025-04-03 14:53:16 -0700

    fix: typo

--- layouts/default.vue
+++ layouts/default.vue
@@ -228,11 +228,11 @@ const links = [
 
     <slot />
 
-    <div class="fixed bottom-1 z-50 mb-0 w-full text-center text-xs">
-      <div class="bg-blue-200 p-2 text-gray-600">
-        This repository is under review for potential modification in compliance
-        with Administration directives.
-      </div>
+    <div
+      class="fixed bottom-0 z-50 mb-0 w-full bg-blue-200 p-2 text-center text-xs text-gray-600"
+    >
+      This repository is under review for potential modification in compliance
+      with Administration directives.
     </div>
   </div>
 </template>
