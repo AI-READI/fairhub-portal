f2cdb1e601ad58d9701749ebea9434fd719002cc
Aydan <62059163+Aydawka@users.noreply.github.com>
2025-04-03 18:15:45 -0700

    feat: ✨ add a notice banner for the repository (#181)
    * dev: add insert script
    
    * feat: add banner (#180)
    
    * feat: add banner
    
    * fix: typo
    
    * fix: typo
    
    * fix: overflow of the about page
    
    * fix: adjust text color
    
    * fix: adjust overflow
    
    * fix: adjust text size
    
    ---------
    
    Co-authored-by: Sanjay Soundarajan <sanjay.16@live.com>

--- scripts/insert.ts
+++ scripts/insert.ts
@@ -1,101 +1,55 @@
-import { MongoClient } from "mongodb";
+import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
+import DatasetRecord from "../dev/datasetRecord.json";
 
-const func = async () => {
-  const mongodbClient = new MongoClient(process.env.MONGODB_URI);
-  await mongodbClient.connect();
-
-  const db = mongodbClient.db(process.env.MONGODB_DB);
-
-  const datasetDescription = [
-    {
-      creatorName: "The University Of Manchester",
-      nameIdentifier: [
-        {
-          nameIdentifierScheme: "ORCID",
-          nameIdentifierValue: "0000-0002-4306-4464",
-          schemeURI: "https://orcid.org",
-        },
-      ],
-      nameType: "Organizational",
-    },
-    {
-      affiliation: [
-        {
-          affiliationIdentifier: "0000-0002-4306-4464",
-          affiliationIdentifierScheme: "ORCID",
-          affiliationValue: "University Of Marlen",
-          schemeURI: "https://orcid.org",
-        },
-      ],
-      creatorName: "Bertoit Hoover",
-      nameIdentifier: [
-        {
-          nameIdentifierScheme: "ROR",
-          nameIdentifierValue: "https://ror.org/04wxnsj81",
-          schemeURI: "https://ror.org",
-        },
-      ],
-      nameType: "Personal",
-    },
-    {
-      affiliation: [
-        {
-          affiliationIdentifier: "0000-0002-4306-4464",
-          affiliationIdentifierScheme: "ORCID",
-          affiliationValue: "University Of Marlen",
-          schemeURI: "https://orcid.org",
-        },
-      ],
-      creatorName: "Bertoit Hoover",
-      nameIdentifier: [
-        {
-          nameIdentifierScheme: "ROR",
-          nameIdentifierValue: "https://ror.org/04wxnsj81",
-          schemeURI: "https://ror.org",
-        },
-      ],
-      nameType: "Personal",
-    },
-    {
-      affiliation: [
-        {
-          affiliationIdentifier: "0000-0002-4306-4464",
-          affiliationIdentifierScheme: "SCHEME TEST",
-          affiliationValue: "University Of Marlen",
-          schemeURI: "https://scheme.org",
-        },
-      ],
-      creatorName: "Bertoit Hoover",
-      nameIdentifier: [
-        {
-          nameIdentifierScheme: "UHHHH",
-          nameIdentifierValue: "https://scheme.org/04wxnsj81",
-          schemeURI: "https://scheme.org",
-        },
-      ],
-      nameType: "Personal",
-    },
-  ];
-
-  // Add the study_description to the metadata field of the dataset
-  return await db.collection("dataset").updateOne(
-    {
-      identifier: 2,
-    },
-    {
-      $set: {
-        "metadata.dataset_description.Creator": datasetDescription,
-      },
-    },
-  );
-};
+const prisma = new PrismaClient();
 
 const main = async () => {
-  await func();
+  const { DATABASE_URL } = process.env;
+
+  // if the domain is from azure exit the function
+  if (!DATABASE_URL) {
+    return;
+  }
+
+  const databaseOnAzure = DATABASE_URL.includes("azure");
+
+  if (databaseOnAzure) {
+    console.log("Azure database detected, exiting function");
+    return;
+  }
+
+  const recordsExist = await prisma.published_dataset.findMany();
+
+  if (recordsExist.length > 0) {
+    console.log("Records exist, exiting function");
+  }
+
+  await prisma.published_dataset.create({
+    data: {
+      id: DatasetRecord.id,
+      title: DatasetRecord.title,
+      created_at: parseInt(DatasetRecord.created_at),
+      data: DatasetRecord.data,
+      dataset_id: DatasetRecord.dataset_id,
+      description: DatasetRecord.description,
+      doi: DatasetRecord.doi,
+      files: DatasetRecord.files,
+      published_metadata: DatasetRecord.published_metadata,
+      study_id: DatasetRecord.study_id,
+      study_title: DatasetRecord.study_title,
+      version_id: DatasetRecord.version_id,
+      version_title: DatasetRecord.version_title,
+    },
+  });
+
+  console.log("Record inserted", DatasetRecord);
 };
 
-main().catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
