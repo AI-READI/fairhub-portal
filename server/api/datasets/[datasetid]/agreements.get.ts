fd8c78f9d4aea8d30d8573ac3794957b647235e5
aydawka <aydan.gasimova2@gmail.com>
2025-08-05 11:03:40 -0700

    wip: sql query

--- server/api/datasets/[datasetid]/agreements.get.ts
+++ server/api/datasets/[datasetid]/agreements.get.ts
@@ -8,6 +8,19 @@ import dayjs from "dayjs";
  * @param defaultValue default value to return if the parameter cannot be parsed
  * @returns the parsed numeric value of the parameter, or the default value
  */
+
+type Agreement = {
+  id: string;
+  affiliation?: string;
+  created_at: string;
+  dataset_id?: string;
+  family_name: string;
+  given_name: string;
+  organization: string;
+  research_purpose: string;
+  version_title: string;
+};
+
 function parseNumericParamWithDefault(
   param: QueryValue,
   defaultValue: number,
@@ -26,6 +39,9 @@ export default defineEventHandler(async (event) => {
   const query = getQuery(event);
   const page = parseNumericParamWithDefault(query.page, 1);
   const pageSize = parseNumericParamWithDefault(query.pageSize, 10);
+
+  const filteredWords = query.filteredWord?.toString().trim().toLowerCase();
+  const shouldSearch = !!filteredWords?.trim();
 
   if (!datasetId) {
     throw createError({
@@ -48,35 +64,46 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  const selectProps = {
-    id: true,
-    dataset_id: true,
-    download_request: {
-      select: {
-        approval: {
-          select: {
-            updated_on: true,
-          },
-        },
-      },
-    },
-    download_user_details: {
-      select: {
-        id: true,
-        affiliation: true,
-        family_name: true,
-        given_name: true,
-        organization: true,
-      },
-    },
-    published_dataset: {
-      select: {
-        version_title: true,
-      },
-    },
-    research_purpose: true,
-  };
+  // const selectProps = {
+  //   id: true,
+  //   dataset_id: true,
+  //   download_request: {
+  //     select: {
+  //       approval: {
+  //         select: {
+  //           updated_on: true,
+  //         },
+  //       },
+  //     },
+  //   },
+  //   download_user_details: {
+  //     select: {
+  //       id: true,
+  //       affiliation: true,
+  //       family_name: true,
+  //       given_name: true,
+  //       organization: true,
+  //     },
+  //   },
+  //   published_dataset: {
+  //     select: {
+  //       version_title: true,
+  //     },
+  //   },
+  //   research_purpose: true,
+  // };
 
+  const selectProps = [
+    "da.id::text AS id",
+    "da.created_at::text AS created_at",
+    "da.research_purpose",
+    "dud.given_name",
+    "dud.family_name",
+    "dud.organization",
+    "pd.version_title",
+    "a.updated_on::text AS updated_on",
+
+  ];
   const totalCount = await prisma.download_agreement.count({
     where: {
       download_request: {
@@ -86,51 +113,121 @@ export default defineEventHandler(async (event) => {
           },
         },
       },
+      ...(filteredWords
+        ? {
+            OR: [
+              {
+                download_user_details: {
+                  given_name: {
+                    contains: filteredWords,
+                    mode: "insensitive",
+                  },
+                },
+              },
+              {
+                download_user_details: {
+                  family_name: {
+                    contains: filteredWords,
+                    mode: "insensitive",
+                  },
+                },
+              },
+              {
+                download_user_details: {
+                  affiliation: {
+                    contains: filteredWords,
+                    mode: "insensitive",
+                  },
+                },
+              },
+              {
+                download_user_details: {
+                  organization: {
+                    contains: filteredWords,
+                    mode: "insensitive",
+                  },
+                },
+              },
+              {
+                published_dataset: {
+                  version_title: {
+                    contains: filteredWords,
+                    mode: "insensitive",
+                  },
+                },
+              },
+              {
+                research_purpose: {
+                  contains: filteredWords,
+                  mode: "insensitive",
+                },
+              },
+            ],
+          }
+        : {}),
     },
   });
+  const offset = (page - 1) * pageSize;
 
-  const agreements = await prisma.download_agreement.findMany({
-    orderBy: [
-      {
-        download_user_details: {
-          family_name: "asc",
-        },
-      },
-      {
-        download_user_details: {
-          given_name: "asc",
-        },
-      },
-      {
-        id: "asc",
-      },
-    ],
-    select: selectProps,
-    skip: (page - 1) * pageSize,
-    take: pageSize,
-    where: {
-      download_request: {
-        some: {
-          approval: {
-            approval_status: "APPROVED",
-          },
-        },
-      },
-    },
-  });
+  const agreements = await prisma.$queryRawUnsafe(`
+  SELECT
+    ${selectProps.join(", ")}
+  FROM
+    download.download_agreement da
+    LEFT JOIN download.download_user_details dud ON da.user_details_id = dud.id
+    LEFT JOIN public.published_dataset pd ON da.dataset_id = pd.id
+    LEFT JOIN download.download_request dr ON dr.download_agreement_id = da.id
+    LEFT JOIN download.download_request_approval a ON dr.approval_id = a.id
+  WHERE
+    a.approval_status = 'APPROVED'
+    ${
+      filteredWords
+        ? `AND (
+        LOWER(dud.given_name) LIKE LOWER('%${filteredWords}%') OR
+        LOWER(dud.family_name) LIKE LOWER('%${filteredWords}%') OR
+        LOWER(dud.affiliation) LIKE LOWER('%${filteredWords}%') OR
+        LOWER(dud.organization) LIKE LOWER('%${filteredWords}%') OR
+        LOWER(pd.version_title) LIKE LOWER('%${filteredWords}%') OR
+        LOWER(da.research_purpose) LIKE LOWER('%${filteredWords}%') OR 
+        TO_CHAR(TO_TIMESTAMP(a.updated_on::bigint), 'Month YYYY') LIKE '%${filteredWords}%'
+      )`
+        : ""
+    }
+    ORDER BY
+    a.updated_on DESC,
+    dud.family_name ASC,
+    dud.given_name ASC
+  LIMIT ${pageSize} OFFSET ${(page - 1) * pageSize};
+`);
+
+  function serializeBigInts<T>(obj: T): T {
+    if (Array.isArray(obj)) {
+      return obj.map(serializeBigInts) as T;
+    } else if (obj && typeof obj === "object") {
+      return Object.fromEntries(
+        Object.entries(obj).map(([key, value]) => [
+          key,
+          typeof value === "bigint"
+            ? value.toString()
+            : serializeBigInts(value),
+        ]),
+      ) as T;
+    }
+    return obj;
+  }
+  const serializedRows = serializeBigInts(agreements) as Agreement[];
 
   return {
-    data: agreements.map((item) => ({
-      id: item.id,
-      name: `${item.download_user_details.given_name} ${item.download_user_details.family_name}`,
-      affiliation: item.download_user_details.affiliation,
-      approval_date: dayjs
-        .unix(Number(BigInt(item.download_request[0].approval.updated_on)))
-        .format("MMM D, YYYY"),
-      dataset_id: item.dataset_id,
-      organization: item.download_user_details.organization,
-      research_purpose: item.research_purpose,
-      version_title: item.published_dataset.version_title,
+    data: serializedRows.map((row: any) => ({
+      id: row.id,
+      name: `${row.given_name} ${row.family_name}`,
+      affiliation: row.affiliation, // if available, otherwise remove
+      approval_date: dayjs.unix(Number(row.updated_on)).format("MMM D, YYYY"),
+
+      dataset_id: row.dataset_id, // if available
+      organization: row.organization,
+      research_purpose: row.research_purpose,
+      version_title: row.version_title,
     })),
     page,
     pageSize,
