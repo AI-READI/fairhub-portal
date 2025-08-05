2d494b31e8e0f9910b2e90f13fa0ad1f2276fdc5
aydawka <aydan.gasimova2@gmail.com>
2025-08-05 11:53:14 -0700

    wip: sql query

--- server/api/datasets/[datasetid]/agreements.get.ts
+++ server/api/datasets/[datasetid]/agreements.get.ts
@@ -1,13 +1,7 @@
+import { sqltag, raw } from "@prisma/client/runtime/library";
 import type { QueryValue } from "ufo";
 import dayjs from "dayjs";
-
-/**
- * Attempts to parse the value of the given query parameter to a numeric value.
- *
- * @param param query parameter value
- * @param defaultValue default value to return if the parameter cannot be parsed
- * @returns the parsed numeric value of the parameter, or the default value
- */
+const sql = sqltag;
 
 type Agreement = {
   id: string;
@@ -26,11 +20,9 @@ function parseNumericParamWithDefault(
   defaultValue: number,
 ): number {
   let parsedValue = NaN;
-
   if (typeof param === "string") {
     parsedValue = Number.parseInt(param);
   }
-
   return Number.isFinite(parsedValue) ? parsedValue : defaultValue;
 }
 
@@ -64,35 +56,6 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  // const selectProps = {
-  //   id: true,
-  //   dataset_id: true,
-  //   download_request: {
-  //     select: {
-  //       approval: {
-  //         select: {
-  //           updated_on: true,
-  //         },
-  //       },
-  //     },
-  //   },
-  //   download_user_details: {
-  //     select: {
-  //       id: true,
-  //       affiliation: true,
-  //       family_name: true,
-  //       given_name: true,
-  //       organization: true,
-  //     },
-  //   },
-  //   published_dataset: {
-  //     select: {
-  //       version_title: true,
-  //     },
-  //   },
-  //   research_purpose: true,
-  // };
-
   const selectProps = [
     "da.id::text AS id",
     "da.created_at::text AS created_at",
@@ -102,8 +65,8 @@ export default defineEventHandler(async (event) => {
     "dud.organization",
     "pd.version_title",
     "a.updated_on::text AS updated_on",
-
   ];
+
   const totalCount = await prisma.download_agreement.count({
     where: {
       download_request: {
@@ -113,31 +76,22 @@ export default defineEventHandler(async (event) => {
           },
         },
       },
-      ...(filteredWords
+      ...(shouldSearch
         ? {
             OR: [
               {
                 download_user_details: {
-                  given_name: {
-                    contains: filteredWords,
-                    mode: "insensitive",
-                  },
+                  given_name: { contains: filteredWords, mode: "insensitive" },
                 },
               },
               {
                 download_user_details: {
-                  family_name: {
-                    contains: filteredWords,
-                    mode: "insensitive",
-                  },
+                  family_name: { contains: filteredWords, mode: "insensitive" },
                 },
               },
               {
                 download_user_details: {
-                  affiliation: {
-                    contains: filteredWords,
-                    mode: "insensitive",
-                  },
+                  affiliation: { contains: filteredWords, mode: "insensitive" },
                 },
               },
               {
@@ -167,38 +121,37 @@ export default defineEventHandler(async (event) => {
         : {}),
     },
   });
+
   const offset = (page - 1) * pageSize;
 
-  const agreements = await prisma.$queryRawUnsafe(`
-  SELECT
-    ${selectProps.join(", ")}
-  FROM
-    download.download_agreement da
+  const agreements = await prisma.$queryRaw<Agreement[]>(
+    sql`
+    SELECT ${raw(selectProps.join(", "))}
+    FROM download.download_agreement da
     LEFT JOIN download.download_user_details dud ON da.user_details_id = dud.id
     LEFT JOIN public.published_dataset pd ON da.dataset_id = pd.id
     LEFT JOIN download.download_request dr ON dr.download_agreement_id = da.id
     LEFT JOIN download.download_request_approval a ON dr.approval_id = a.id
-  WHERE
-    a.approval_status = 'APPROVED'
+    WHERE a.approval_status = 'APPROVED'
     ${
-      filteredWords
-        ? `AND (
-        LOWER(dud.given_name) LIKE LOWER('%${filteredWords}%') OR
-        LOWER(dud.family_name) LIKE LOWER('%${filteredWords}%') OR
-        LOWER(dud.affiliation) LIKE LOWER('%${filteredWords}%') OR
-        LOWER(dud.organization) LIKE LOWER('%${filteredWords}%') OR
-        LOWER(pd.version_title) LIKE LOWER('%${filteredWords}%') OR
-        LOWER(da.research_purpose) LIKE LOWER('%${filteredWords}%') OR 
-        TO_CHAR(TO_TIMESTAMP(a.updated_on::bigint), 'Month YYYY') LIKE '%${filteredWords}%'
-      )`
-        : ""
+      shouldSearch
+        ? sql`
+        AND (
+          LOWER(dud.given_name) LIKE LOWER(${`%${filteredWords}%`}) OR
+          LOWER(dud.family_name) LIKE LOWER(${`%${filteredWords}%`}) OR
+          LOWER(dud.affiliation) LIKE LOWER(${`%${filteredWords}%`}) OR
+          LOWER(dud.organization) LIKE LOWER(${`%${filteredWords}%`}) OR
+          LOWER(pd.version_title) LIKE LOWER(${`%${filteredWords}%`}) OR
+          LOWER(da.research_purpose) LIKE LOWER(${`%${filteredWords}%`}) OR
+          TO_CHAR(TO_TIMESTAMP(a.updated_on::bigint), 'FMMonth FMDD, YYYY') ILIKE ${`%${filteredWords}%`}
+        )`
+        : sql``
     }
-    ORDER BY
-    a.updated_on DESC,
-    dud.family_name ASC,
-    dud.given_name ASC
-  LIMIT ${pageSize} OFFSET ${(page - 1) * pageSize};
-`);
+    ORDER BY a.updated_on DESC, dud.family_name ASC, dud.given_name ASC
+    LIMIT ${pageSize}
+    OFFSET ${offset}
+  `,
+  );
 
   function serializeBigInts<T>(obj: T): T {
     if (Array.isArray(obj)) {
@@ -215,16 +168,16 @@ export default defineEventHandler(async (event) => {
     }
     return obj;
   }
+
   const serializedRows = serializeBigInts(agreements) as Agreement[];
 
   return {
     data: serializedRows.map((row: any) => ({
       id: row.id,
       name: `${row.given_name} ${row.family_name}`,
-      affiliation: row.affiliation, // if available, otherwise remove
+      affiliation: row.affiliation,
       approval_date: dayjs.unix(Number(row.updated_on)).format("MMM D, YYYY"),
-
-      dataset_id: row.dataset_id, // if available
+      dataset_id: row.dataset_id,
       organization: row.organization,
       research_purpose: row.research_purpose,
       version_title: row.version_title,
