7529a9b998efde49e5526df34094922615ab2500
aydawka <aydan.gasimova2@gmail.com>
2025-08-12 15:46:21 -0700

    fix: add restricted access

--- pages/datasets/[datasetid]/index.vue
+++ pages/datasets/[datasetid]/index.vue
@@ -139,13 +139,20 @@ const NuxtSchemaDataset: WithContext<Dataset> = {
   )?.descriptionValue,
   distribution: dataset.value?.id
     ? [
-        {
-          name: "Dataset Description (JSON)",
+        // {
+        //   name: "Dataset Description (JSON)",
+        //   "@type": "DataDownload",
+        //   contentSize: "55 KB",
+        //   contentUrl: `${config.public.BASE_URL}/api/datasets/${datasetid}/dataset_description.json`,
+        //   encodingFormat: "application/json",
+        // },
+          {
           "@type": "DataDownload",
-          contentSize: "2.01 TB",
-          contentUrl: `${config.public.BASE_URL}/api/datasets/${datasetid}/dataset_description.json`,
-          encodingFormat: "application/json",
-        },
+          "name": dataset.value?.title,
+          "contentUrl": "https://staging.fairhub.io/datasets/2/access",
+          "encodingFormat": "application/zip",
+          "contentSize": "2.01 TB",
+          "description": `${dataset.value?.description} Dataset is accessible only to approved researchers via this landing page.`        }
       ]
     : undefined,
   funder: dataset.value?.metadata.datasetDescription.fundingReference?.map(
