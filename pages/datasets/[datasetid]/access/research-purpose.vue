2b835584906afc774a251fee3eb8a059b3b5a40d
Erik Benton <benton@ohsu.edu>
2025-09-10 11:50:37 -0700

    fix: update formatting with subtext for labels

--- pages/datasets/[datasetid]/access/research-purpose.vue
+++ pages/datasets/[datasetid]/access/research-purpose.vue
@@ -255,10 +255,65 @@ const validForm = computed(
               transparency and trust with the use of this data.
             </p>
 
-            <n-form-item
-              label="Briefly describe the purpose of your research."
-              :rule="validateResearchPurpose"
-            >
+            <n-form-item :rule="validateResearchPurpose">
+              <template #label>
+                <div style="line-height: 2">
+                  <label>Briefly describe the purpose of your research.</label>
+
+                  <br />
+
+                  <span style="opacity: 0.6">
+                    Why is this study being done? Describe how the research
+                    aligns with the research purpose in the original consent
+                    provided to AI-READI study participants.
+                    <n-popover
+                      trigger="hover"
+                      placement="top"
+                      width="500"
+                      :show-arrow="false"
+                    >
+                      <template #trigger>
+                        <Icon
+                          name="ic:outline-info"
+                          color="#2080f0"
+                          size="16"
+                        />
+                      </template>
+
+                      <div>
+                        <div>
+                          Participants were provided the following language in
+                          the consent:
+                        </div>
+
+                        <br />
+
+                        <em>
+                          The purpose of this research program is to collect
+                          health and other personal information from individuals
+                          for future studies on type 2 diabetes. Information
+                          will be collected from people with and without Type 2
+                          diabetes. The type of data collected from you has been
+                          specifically designed to answer research questions
+                          about type 2 diabetes but may also be used by
+                          scientists to study other aspects of health, disease,
+                          and behavior. The data collected in this research
+                          program may be used for future research to learn about
+                          type 2 diabetes, health, disease, and behavior, but
+                          may also be used for other purposes, such as product
+                          development. The data stored in the AI-READI database
+                          will be analyzed in the future using artificial
+                          intelligence/machine learning (AI/ML). Other analysis
+                          techniques may also be used. AI/ML looks for patterns
+                          using very large datasets and can help with new
+                          discoveries in understanding of health and disease.
+                        </em>
+                      </div>
+                    </n-popover>
+                  </span>
+                </div>
+              </template>
+
               <n-input
                 v-model:value="agreementFormState.research_purpose"
                 type="textarea"
@@ -266,7 +321,6 @@ const validForm = computed(
                 :maxlength="maxResearchPurposeLength"
                 :rows="5"
                 show-count
-                placeholder="Why is this study being done? Describe how the research aligns with the research purpose in the original consent provided to AI-READI study participants. **"
               />
             </n-form-item>
 
@@ -292,87 +346,180 @@ const validForm = computed(
                   />
                 </n-form-item>
 
-                <p>
-                  Provide an overview of your study below. This should include
-                  Study objectives; Study design; Description of how the
-                  AI-READI dataset is consistent with the objectives of the
-                  proposed research project; Brief Analysis plan with methods.
-                </p>
+                <n-form-item path="study_overview">
+                  <template #label>
+                    <div style="line-height: 2">
+                      <label>Study Objective</label>
 
-                <n-form-item label="Study Overview" path="study_overview">
+                      <br />
+
+                      <span style="opacity: 0.6">
+                        Clearly outline the specific aims or hypotheses of your
+                        study.
+                      </span>
+                    </div>
+                  </template>
+
                   <n-input
                     v-model:value="agreementFormState.study_overview"
                     type="textarea"
                     :maxlength="maxOverviewLength"
                     :rows="10"
                     show-count
-                    placeholder="Clearly outline the specific aims or hypotheses of your study."
                   />
                 </n-form-item>
 
-                <n-form-item
-                  label="Background and Rationale"
-                  path="study_background"
-                >
+                <n-form-item path="study_background">
+                  <template #label>
+                    <div>
+                      <label>Background and Rationale</label>
+
+                      <br />
+
+                      <span style="opacity: 0.6">
+                        Describe the scientific premise of your research,
+                        including:
+                        <ul>
+                          <li>The importance of the research question.</li>
+
+                          <li>
+                            The potential impact of the study (Please include
+                            potential societal or scientific benefits).
+                          </li>
+
+                          <li>
+                            How the requested data will help you achieve your
+                            objectives.
+                          </li>
+                        </ul>
+                      </span>
+                    </div>
+                  </template>
+
                   <n-input
                     v-model:value="agreementFormState.study_background"
                     type="textarea"
                     :maxlength="maxOverviewLength"
                     :rows="5"
                     show-count
-                    placeholder="Describe the scientific premise of your research, including: 
-The importance of the research question
-The potential impact of the study (Please include potential societal or scientific benefits)
-How the requested data will help you achieve your objectives
-"
                   />
                 </n-form-item>
 
-                <n-form-item label="Study Design" path="study_design">
+                <n-form-item path="study_design">
+                  <template #label>
+                    <div>
+                      <label>Study Design</label>
+
+                      <br />
+
+                      <span style="opacity: 0.6">
+                        Provide details on the methodology, including:
+                        <ul>
+                          <li>
+                            Type of analysis (e.g., statistical, AI/ML- based).
+                          </li>
+
+                          <li>Expected outcomes</li>
+
+                          <li>Timeline for completion.</li>
+                        </ul>
+                      </span>
+                    </div>
+                  </template>
+
                   <n-input
                     v-model:value="agreementFormState.study_design"
                     type="textarea"
                     :maxlength="maxOverviewLength"
                     :rows="5"
                     show-count
-                    placeholder="Provide details on the methodology, including:
-Type of analysis (e.g., statistical, AI/ML- based)
-Expected outcomes
-Timeline for completion"
                   />
                 </n-form-item>
 
-                <n-form-item
-                  label="Data Handling: For data security and storage"
-                  path="data_handling_plan"
-                >
+                <n-form-item path="data_handling_plan">
+                  <template #label>
+                    <div>
+                      <label
+                        >Data Handling: For data security and storage</label
+                      >
+
+                      <br />
+
+                      <span style="opacity: 0.6">
+                        <ul>
+                          <li>
+                            Describe where and how the data will be stored
+                            (e.g., secure servers, encrypted drives).
+                          </li>
+
+                          <li>
+                            Specify who will have access to the data and how
+                            access will be controlled.
+                          </li>
+
+                          <li>
+                            Include retention period and plans for data disposal
+                            after project completion (This will be covered in
+                            the DUA).
+                          </li>
+                        </ul>
+                      </span>
+                    </div>
+                  </template>
+
                   <n-input
                     v-model:value="agreementFormState.data_handling_plan"
                     type="textarea"
                     :maxlength="maxOverviewLength"
                     :rows="5"
                     show-count
-                    placeholder="Describe where and how the data will be stored (e.g., secure servers, encrypted drives)
-Specify who will have access to the data and how access will be controlled
-Include retention period and plans for data disposal after project completion (This will be covered in the DUA)
-"
                   />
                 </n-form-item>
 
-                <n-form-item label="AI" path="ai_use">
+                <n-form-item path="ai_use">
+                  <template #label>
+                    <div>
+                      <label>AI</label>
+
+                      <br />
+
+                      <span style="opacity: 0.6">
+                        <ul>
+                          <li>
+                            Does the project plan to use AI/ML? If so, please
+                            describe model being used.
+                          </li>
+
+                          <li>
+                            Please provide the roles and qualifications of team
+                            members who will be conducting the analysis.
+                          </li>
+
+                          <li>
+                            How are you ensuring fairness and avoiding bias in
+                            the outputs of the model?
+                          </li>
+
+                          <li>
+                            Is the model being run locally? If not, please
+                            describe Is there a vendor involved?
+                          </li>
+
+                          <li>
+                            How is potential data memorization being addressed
+                            by the model?
+                          </li>
+                        </ul>
+                      </span>
+                    </div>
+                  </template>
+
                   <n-input
                     v-model:value="agreementFormState.ai_use"
                     type="textarea"
                     :maxlength="maxOverviewLength"
                     :rows="5"
                     show-count
-                    placeholder="Does the project plan to use AI/ML? If so, please describe model being used
-Please provide the roles and qualifications of team members who will be conducting the analysis 
-How are you ensuring fairness and avoiding bias in the outputs of the model?
-Is the model being run locally? If not, please describe
-Is there a vendor involved?
-How is potential data memorization being addressed by the model?
-"
                   />
                 </n-form-item>
 
@@ -464,17 +611,24 @@ How is potential data memorization being addressed by the model?
                   />
                 </n-form-item>
 
-                <p>
-                  Will Collaborators/Data Users outside of your institution have
-                  access to the data? If yes, they will be required to submit a
-                  separate DAR for review and approval and approved user and
-                  their institution will be required to sign a separate DUA.
-                </p>
+                <n-form-item path="collaborator_data_access">
+                  <template #label>
+                    <div style="line-height: 2">
+                      <label>Will collaborators have access to the data?</label>
 
-                <n-form-item
-                  label="Will collaborators have access to the data?"
-                  path="collaborator_data_access"
-                >
+                      <br />
+
+                      <span style="opacity: 0.6">
+                        Will Collaborators/Data Users outside of your
+                        institution have access to the data? If yes, they will
+                        be required to submit a separate Data Access Request
+                        (DAR) for review and approval and approved user and
+                        their institution will be required to sign a separate
+                        DUA.
+                      </span>
+                    </div>
+                  </template>
+
                   <n-radio-group
                     v-model:value="agreementFormState.collaborator_data_access"
                   >
