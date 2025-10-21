0cd42e22ff5be88e44e28b8a3877cbdd82e65537
Erik Benton <kirenotneb@users.noreply.github.com>
2025-10-20 19:04:03 -0700

    Update link to Azure documentation for dataset access

--- pages/datasets/[datasetid]/access/index.vue
+++ pages/datasets/[datasetid]/access/index.vue
@@ -156,7 +156,7 @@ const allChecked = computed(() =>
                     in order to receive the dataset due to the size of the
                     AI-READI dataset. Please
                     <NuxtLink
-                      to="https://docs.aireadi.org/docs/2/preliminary/azure"
+                      to="https://docs.aireadi.org/docs/2/preliminary/azure_storage/azure"
                       >review our documentation</NuxtLink
                     >
                     to set this up.
