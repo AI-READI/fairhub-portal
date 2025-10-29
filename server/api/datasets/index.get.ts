8a2a824a4a2fda5ddf3bac0b35a69d80343be001
aydawka <aydan.gasimova2@gmail.com>
2025-10-29 11:14:24 -0700

    fix: update column for order

--- server/api/datasets/index.get.ts
+++ server/api/datasets/index.get.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (_event) => {
   const publishedDatasets = await prisma.published_dataset.findMany({
     distinct: ["dataset_id"],
-    orderBy: [{ dataset_id: "asc" }, { created_at: "desc" }],
+    orderBy: [{ id: "desc" }, { created_at: "desc" }],
   });
 
   if (!publishedDatasets) {
