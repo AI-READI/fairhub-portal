dd402d5be4fdd780b764835ec80f54b93c7be200
aydawka <aydan.gasimova2@gmail.com>
2025-10-28 22:39:25 -0700

    fix: order dataset id

--- server/api/datasets/index.get.ts
+++ server/api/datasets/index.get.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (_event) => {
   const publishedDatasets = await prisma.published_dataset.findMany({
     distinct: ["dataset_id"],
-    orderBy: [{ created_at: "asc" }, { dataset_id: "desc" }],
+    orderBy: [{ dataset_id: "desc" }, { created_at: "desc" }],
   });
 
   if (!publishedDatasets) {
